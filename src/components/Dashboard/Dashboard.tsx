7d492c05499e3b4b161991adf1d943573872aa39
gaebar <gaetanobarreca@gmail.com>
2024-06-19 16:30:36 -0700

    Update dashboard title to "React Task Suite"

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ import './Dashboard.css';
 const Dashboard: React.FC = () => {
   return (
     <div className="dashboard-container">
-      <h1>Angular/React Task</h1>
+      <h1>React Task Suite</h1>
       <div className="buttons-container">
         <Link to="/task1"><button className="task-button">Task 1</button></Link>
         <Link to="/task2"><button className="task-button">Task 2</button></Link>
